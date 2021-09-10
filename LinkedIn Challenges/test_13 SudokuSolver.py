a64a5a908e7bf30c775d26e1f115003209ea89ea
peternewman22 <peter.newman.22@gmail.com>
2021-09-10 15:52:22 +1000

    Structure complete - work out the backtracking

--- LinkedIn Challenges/test_13 SudokuSolver.py
+++ LinkedIn Challenges/test_13 SudokuSolver.py
@@ -2,6 +2,8 @@
 Function takes a list and recursively solves the sudoku
 
 """
+to_check = range(1,10)
+
 puzzle = [[5,3,0,0,7,0,0,0,0],
           [6,0,0,1,9,5,0,0,0],
           [0,9,8,0,0,0,0,6,0],
@@ -13,8 +15,29 @@ puzzle = [[5,3,0,0,7,0,0,0,0],
           [0,0,0,0,8,0,0,7,9]
         ]
 
-def solve(puzzle: list) -> list:
-    to_check = range(1,10)
+solution = [[5,3,4,6,7,8,9,1,2],
+            [6,7,2,1,9,5,3,4,8],
+            [1,9,8,3,4,2,5,6,7],
+            [8,5,9,7,6,1,4,2,3],
+            [4,2,6,8,5,3,7,9,1],
+            [7,1,3,9,2,4,8,5,6],
+            [9,6,1,5,3,7,2,8,4],
+            [2,8,7,4,1,9,6,3,5],
+            [3,4,5,2,8,6,1,7,9]]
+
+
+def solve(puzzle: list, start_row: int, start_col: int) -> list:
+    
+    # solution = puzzle
+    for each_row in range(start_row, 10):
+        for each_col in range(start_col, 10):
+            if puzzle[each_row][each_col] == 0:
+
+
+    # go through the puzzle and
+    # return solution
+
+
 
 def valid_column_entry(col: int, candidate: int) -> bool:
     for eachRow in puzzle:
@@ -28,12 +51,12 @@ def valid_row_entry(row: int, candidate: int) -> bool:
 def valid_square_entry(row: int, col: int, candidate: int) -> bool:
     square_row = int(row/3)
     square_col = int(col/3)
-    square = [puzzle[row][col:col+3],
-            puzzle[row+1][col:col+3],
-            puzzle[row+2][col:col+3]]
-    for each_row in square:
-        print(each_row)
-    print("\n")
+    # square = [puzzle[row][col:col+3],
+    #         puzzle[row+1][col:col+3],
+    #         puzzle[row+2][col:col+3]]
+    # for each_row in square:
+    #     print(each_row)
+    # print("\n")
 
     for i in range(square_row*3, square_row*3+3):
         for j in range(square_col*3, square_col*3+3):
@@ -41,6 +64,8 @@ def valid_square_entry(row: int, col: int, candidate: int) -> bool:
                 return False
     return True
 
+def is_valid(row: int, col: int, candidate: int) -> bool:
+    return valid_square_entry(row,col,candidate) and valid_column_entry(col,candidate) and valid_row_entry(row, candidate)
 
 # solve(puzzle)
 
@@ -66,3 +91,9 @@ def test_valid_square_entry():
     assert valid_square_entry(6,6,2) == False
     assert valid_square_entry(6,6,9) == False
     assert valid_square_entry(6,6,3) == True
+
+# def test_is_valid():
+#     assert is_valid(1,1,1) == False
+#     assert is_valid(1,1,1) == False
+# def test_check_solution():
+#     assert solve(puzzle) == solution
